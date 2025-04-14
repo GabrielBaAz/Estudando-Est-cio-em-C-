392c351fd10046dbfee81df7e1138fe214a9cb97
GabrielBaAz <Gabrielbarrosazevedo951@gmail.com>
2025-04-14 20:47:11 -0300

    Adição de trabalhos

---
+++ Divisão por 2 e resto da divisão por 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int  metade_inteira, resto, valor;
+	
+	cout <<"Escreva um valor: "; 
+	cin >> valor; 
+	
+	metade_inteira = valor / 2 ;
+	resto =  valor % 3;
+	
+	cout << " a metade inteira do numero e : " << metade_inteira <<  endl;
+	cout << "o resto da divisao por 3 e : " << resto << endl;
+	 
+return 0;
+
+}
+		
---
+++ Leia um número e faça a tabuada.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Leia um número e imprima a tabuada de multiplicação desse número
+	 
+	 int i, numero;
+	 cout << "Escreva um numero: ";
+	 cin >> numero;
+	 
+	  for(i=1; i<=10; ++i)	
+	  {
+	  	cout << numero << "*" << i << "=" << numero * i << endl; 
+	  }
+	  	return 0; 
+	  
+	  
+	  
+	  }
+
+	
+
